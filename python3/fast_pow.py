ac782b78271be3180e84f6f7e7bcfd5788740b98
jshyun912 <108142462+jshyun912@users.noreply.github.com>
2023-01-20 02:05:52 +0900

    Update fast_pow.py

--- python3/fast_pow.py
+++ python3/fast_pow.py
@@ -1,3 +1,5 @@
+#분할정복을 이용한 거듭제곱
+
 def POW(a, b):
     res = 1
 
@@ -9,15 +11,15 @@ def POW(a, b):
 
     return res
   
-  def POW(a, b, c):
-    res = 1
+def POW(a, b, c):
+  res = 1
+  
+  while b > 0:
+    if b % 2 != 0:
+      res *= a
+      res %= c
+    a *= a
+    a %= c
+    b //= 2
 
-    while b > 0:
-        if b % 2 != 0:
-            res *= a
-            res %= c
-        a *= a
-        a %= c
-        b //= 2
-
-    return res
+  return res
