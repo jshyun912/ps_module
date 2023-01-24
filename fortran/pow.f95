51d7276af185721a5c849bc4c8a5518bec19407d
jshyun912 <108142462+jshyun912@users.noreply.github.com>
2023-01-24 23:57:22 +0900

    Update pow.f95

--- fortran/pow.f95
+++ fortran/pow.f95
@@ -1,17 +1,33 @@
 !분할정복을 이용한 거듭제곱
 
-function pow(a, b, div) result(res)
+!나머지 연산 없는 버전
+function pow(a, b) result(res)
     implicit none
-    integer*16 a, b, d, e, div, res
-    d = a 
-    e = b 
-    res = 1
-    d = mod(d, div)
+    integer*16 :: a, b, res
 
-    do while (e > 0)
-        if (mod(e, 2) /= 0) res = mod((d * res), div)
-        d = mod(d * d, div)
-        e = e / 2
+    res = 1
+
+    do while (b > 0)
+        if (mod(b, 2) /= 0) res = res * a
+        a = a * a
+        b = b / 2
+    end do
+
+    return
+end
+
+!나머지 연산 있는 버전
+
+function pow(a, b, c) result(res)
+    implicit none
+    integer*16 :: a, b, c, res
+
+    res = 1
+
+    do while (b > 0)
+        if (mod(b, 2) /= 0) res = mod(res * a, c)
+        a = mod(a * a, c)
+        b = b / 2
     end do
 
     return
