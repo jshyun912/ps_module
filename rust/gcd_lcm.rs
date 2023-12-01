7d1e7a476714709d31924234d91fb8eea6c0d921
jshyun912 <108142462+jshyun912@users.noreply.github.com>
2023-12-01 19:07:51 +0900

    Create gcd_lcm.rs

---
+++ rust/gcd_lcm.rs
@@ -0,0 +1,15 @@
+fn gcd(mut a:usize, mut b:usize) -> usize
+{
+    let mut tmp;
+
+    while b > 0
+    {
+        tmp = a;
+        a = b;
+        b = tmp % b;
+    }
+
+    a
+}
+
+fn lcm(a:usize, b:usize) -> usize { a * b / gcd(a, b) }
