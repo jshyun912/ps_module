4e33a6c0393133c7063cce245b131b0b981ea640
jshyun912 <108142462+jshyun912@users.noreply.github.com>
2024-03-04 20:52:44 +0900

    Update convex_hull.py

--- python3/convex_hull.py
+++ python3/convex_hull.py
@@ -1,29 +1,21 @@
 #볼록 껍질 알고리즘
 
-def ccw(a, b, c):
-    return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
+def ccw(a, b, c): return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
 
-coord.sort()
-
-up = deque()
-down = deque()
-
-up.append(coord[0]); up.append(coord[1])
-down.append(coord[0]); down.append(coord[1])
-
-for i in range(2, N):
-    if coord[i] == up[-1]:
-        continue
+def convex_hull(coord):
+    if len(coord) == 1: return coord
+    
+    up = []
+    down = []
+    
+    for i in coord:
+        while len(up) >= 2 and ccw(up[len(up) - 2], up[len(up) - 1], i) >= 0: up.pop()
+        up.append(i)
         
-    while len(up) > 1:
-        if ccw(up[-2], up[-1], coord[i]) <= 0:
-            break
-        up.pop()
-
-    while len(down) > 1:
-        if ccw(down[-2], down[-1], coord[i]) >= 0:
-            break
-        down.pop()
-
-    up.append(coord[i])
-    down.append(coord[i])
+    for i in reversed(coord):
+        while len(down) >= 2 and ccw(down[len(down) - 2], down[len(down) - 1], i) >= 0: down.pop()
+        down.append(i)
+    
+    up.reverse(); down.reverse()
+    
+    return down[:-1] + up[:-1]
