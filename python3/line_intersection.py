b7d193ae92ae0620187be797c24a219a72feb9de
jshyun912 <108142462+jshyun912@users.noreply.github.com>
2024-03-04 16:35:40 +0900

    Create line_intersection.py

---
+++ python3/line_intersection.py
@@ -0,0 +1,37 @@
+def cross(a, b): return a[0] * b[1] - a[1] * b[0]  # 외적
+def vect(a, b): return (a[0] - b[0], a[1] - b[1]) # 벡터 계산 (b에서 a로 이동)
+
+def line_intersection(p0, p1, q0, q1):
+    q0_q1 = vect(q1, q0)
+    p0_p1 = vect(p1, p0)
+    p0_q0 = vect(q0, p0)
+    
+    par = cross(p0_p1, q0_q1) # 두 직선이 평행한가요?
+    
+    if par != 0: # 평행하지 않다면
+        s = [cross(p0_q0, q0_q1), abs(par)]
+        t = [cross(p0_q0, p0_p1), abs(par)]
+        
+        if par < 0: s[0], t[0] = -s[0], -t[0]
+        
+        if s[0] < 0 or s[0] > s[1] or t[0] < 0 or t[0] > t[1]: return 0 # 교점이 없다
+        if s[0] == 0 or s[0] == s[1] or t[0] == 0 or t[0] == t[1]: return 1 # 교점이 한 선분의 끝
+        else: return 2 # 교점이 하나
+    
+    p1_q0 = vect(q0, p1)
+    p1_q1 = vect(q1, p1)
+
+   # 선분이 한 직선위에 있는가?
+    if cross(p0_p1, p1_q0) or cross(p0_p1, p1_q1) or cross(p0_q0, q0_q1) or cross(p1_q0, q0_q1): 
+        return 0
+    
+    a, b = min(p0, p1), max(p0, p1)
+    c, d = min(q0, q1), max(q0, q1)
+    
+    l, r = max(a, c), min(b, d)
+    
+    if l == r: return 1 # 교점이 한개
+    if l < r: return 3 # 교점이 무한함
+    return 0 # 없음
+
+# TODO : 교점의 좌표를 반환하는 코드 제작
