c6864fcd07831a2ffa864640e698b11a2e103c11
jshyun912 <108142462+jshyun912@users.noreply.github.com>
2024-03-07 03:16:35 +0900

    Update line_intersection.py

--- python3/line_intersection.py
+++ python3/line_intersection.py
@@ -34,4 +34,42 @@ def line_intersection(p0, p1, q0, q1):
     if l < r: return 3 # 교점이 무한함
     return 0 # 없음
 
-# TODO : 교점의 좌표를 반환하는 코드 제작
+# 교점의 좌표를 반환하는 코드
+
+from fractions import Fraction
+
+def cross(a, b): return a[0] * b[1] - a[1] * b[0]
+def vect(a, b): return (a[0] - b[0], a[1] - b[1])
+
+def line_intersection_coord(p0, p1, q0, q1):
+    q0_q1 = vect(q1, q0)
+    p0_p1 = vect(p1, p0)
+    p0_q0 = vect(q0, p0)
+    
+    par = cross(p0_p1, q0_q1)
+    
+    if par != 0:
+        s = Fraction(cross(p0_q0, q0_q1), par)
+        t = Fraction(cross(p0_q0, p0_p1), par)
+        
+        if 0 <= s <= 1 and 0 <= t <= 1:
+            x = p0[0] + (p1[0] - p0[0]) * s
+            y = p0[1] + (p1[1] - p0[1]) * s
+            return (x, y)
+        
+        return
+    
+    p1_q0 = vect(q0, p1)
+    p1_q1 = vect(q1, p1)
+
+    if cross(p0_p1, p1_q0) or cross(p0_p1, p1_q1) or cross(p0_q0, q0_q1) or cross(p1_q0, q0_q1): 
+        return
+    
+    a, b = min(p0, p1), max(p0, p1)
+    c, d = min(q0, q1), max(q0, q1)
+    
+    l, r = max(a, c), min(b, d)
+    
+    if l == r: return l
+    if l < r: return [l, r]
+    return
